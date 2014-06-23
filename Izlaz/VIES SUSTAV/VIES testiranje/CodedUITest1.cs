be390c977226166f63e4934bcb3cb2edf821b5f2
lidija-lastavec <lidija.lastavec@gmail.com>
2014-06-23 11:18:10 +0200

    Code_2

---
+++ Izlaz/VIES SUSTAV/VIES testiranje/CodedUITest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace VIES_testiranje
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.UC101_PrijavaPoreznogObveznika();
+            this.UIMap.UC100_PrijavaZaposlenikaPU();
+            this.UIMap.UC108_FunkcionalnostiZaPoreznogObveznika();
+            this.UIMap.UC108_FunkcionalnostiZaReferentaPU();
+            this.UIMap.UC108_FunkcionalnostiZaAdministratoraPU();
+            this.UIMap.UC200_PregledSifarnika();
+            this.UIMap.UC201_DodavanjeNovogObveznika();
+            this.UIMap.UC202_PretraživanjeRegistraObveznikaIdetaljanPregled();
+            this.UIMap.UC203_AzuriranjePodatakaOporeznomObvezniku();
+            this.UIMap.UC300_UnosViesIzvjestaja();
+            this.UIMap.UC301_PretraživanjeIpregledViesPodataka();
+            this.UIMap.UC302_AzuriranjeVIESizvjestaja();
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //    // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //    // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
